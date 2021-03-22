eabff639a94489cf183f3a24fd4064f989db3cd7
Pmarze <53276103+Pmarze@users.noreply.github.com>
2021-03-21 21:45:32 -0600

    Comentado y finalizado
    Los resultados se extraen en un archivo de texto

--- Computacional/Armstrong.f90
+++ Computacional/Armstrong.f90
@@ -11,6 +11,9 @@
 !    gfortran -Wall -pedantic -std=f95 -c -o Armstrong.o Armstrong.f90
 !    gfortran -o Armstrong.x Armstrong.o
 !    ./Armstrong.x
+!    Para obervar el análisis de tiempo, memoria y procesador, se sustituye 
+!    la linea anterior por la siguiente
+!    /usr/bin/time -f "%e %M %P" ./Armstrong.x
 
 !    Copyright (C) 2021
 !    P. D. Martínez Zeceña
@@ -29,46 +32,43 @@
 !    You should have received a copy of the GNU General Public License
 !    along with this program.  If not, see
 !    <http://www.gnu.org/licenses/>.
+
 PROGRAM Armstrong
     IMPLICIT NONE
-    INTEGER :: npb=100000000, bmin=2, bam=10              ! Numeros por base, base máxima
-    INTEGER :: b, x, k, d, f1,f2 , i              ! Variables del problema
-    REAL :: xreal,breal
-    OPEN(12,file='Armstrong.txt')
+    INTEGER :: npb=1000000000               ! Cantidad de números por base
+    INTEGER :: bmin=4, bam=10               ! Base mínima, Base máxima
+    INTEGER :: b, x, k, d, f , i            ! Variables del problema
+    REAL :: xreal,breal                     ! Necesario para la función que calcula "k"
+    OPEN(12,file='Armstrong.txt')           ! Abre y/o crea el archivo de texto para guardar los datos
     print*, 'números por base=',npb
-    DO b=bmin, bam
-        print*, 'base=',b
-        WRITE(12,*) 'Base=',b
-        DO x=0, npb
+    DO b=bmin, bam                          ! Desde la base mínima hasta la base máxima                  
+        WRITE(12,*) 'Base=',b              
+        DO x=0, npb                         ! Se consideran solo números no negativos
             xreal=REAL(x)
             breal=REAL(b)
-            CALL kb(xreal,breal,k)
-!            IF (k>1)THEN
-                DO i=0, k-1
-                    CALL di(i,x,b,d)
-                    f1=f2
-                    f2=d**k+f1
-                END DO
-            !END IF
-            IF (f2==x)THEN
-                print*,f2,'es un número de Armstrong'
-                WRITE(12,*) f2
+            CALL kb(xreal,breal,k)          ! Se calcula el valor de "k" y se almacena en la variable k
+            DO i=0, k-1                     ! Se realiza una sumatoria desde i=0 hasta k-1
+                CALL di(i,x,b,d)            ! Se calcula el factor d_i y se almacena en la variable d                  
+                f=d**k+f                    ! Se realiza la sumatoria de d_i^k
+            END DO
+            IF (f==x)THEN                   ! Si f=x, este es un número de Armstrong
+                WRITE(12,*) f
             END IF
-            f2=0
+            f=0                             ! Se eliminan los resultados anteriores
         END DO
     END DO
 END PROGRAM Armstrong
 
-SUBROUTINE di(i,n,b,y)
+SUBROUTINE di(i,n,b,y)                      ! Subrutina para encontrar el coeficiente d_i
     INTEGER, INTENT(IN) :: i, n, b
     INTEGER, INTENT(OUT) :: y
-    y=(MOD(n,b**(i+1))-MOD(n,b**i))/(b**i)
+    y=(MOD(n,b**(i+1))-MOD(n,b**i))/(b**i)  ! Fórmula dada para calcular
 END SUBROUTINE di
 
-SUBROUTINE kb(x,b,y)
+SUBROUTINE kb(x,b,y)                        ! Subrutina para encontrar el coeficiente k
     REAL, INTENT(IN) :: b,x
     INTEGER, INTENT(OUT) :: y
     REAL :: z
-    z=LOG(x)/LOG(b)
-    y=INT(z)+1
+    z=LOG(x)/LOG(b)                         ! Formula para el cambio de logaritmo a base b
+    y=INT(z)+1                              ! El resultado es un entero y se devuelve como tal
 END SUBROUTINE kb
