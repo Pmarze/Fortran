1c04200b7f81fb5c04d809ad5c425ead55842ebd
Pmarze <53276103+Pmarze@users.noreply.github.com>
2021-03-25 02:53:09 -0600

    Finalizado y comentado

--- Computacional/ArmstrongParalelo.f90
+++ Computacional/ArmstrongParalelo.f90
@@ -15,7 +15,7 @@
 !    mpirun -np 8 ./ArmstrongParalelo.x
 !    Para obervar el análisis de tiempo, memoria y procesador, se sustituye 
 !    la linea anterior por la siguiente
-!    /usr/bin/time -f "%e %M %P" ./Armstrong.x
+!    /usr/bin/time -f "%e %M %P" ./ArmstrongParalelo.x
 
 !    Copyright (C) 2021
 !    P. D. Martínez Zeceña
@@ -38,160 +38,269 @@
 PROGRAM ArmstrongParalelo
 USE MPI
 IMPLICIT NONE
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    INTEGER :: ierr                                 ! Variable para la señal de error
-    INTEGER :: rank                                 ! ID del núcleo a usar
-    INTEGER :: nprocs                               ! Número de procesadores a usar
-    INTEGER, DIMENSION(MPI_STATUS_SIZE) :: status   ! Estatus de envio o recibido
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    INTEGER :: npb=10                               ! Cantidad de números por base
-    INTEGER :: bmin=1, bama=17                     ! Base mínima, Base máxima
-    !INTEGER :: bmin=1, bama=7                       ! Base mínima, Base máxima
-    !INTEGER :: bmin=1, bama=3                      ! Base mínima, Base máxima
-    INTEGER :: b, x, k, d, f , i, bcal              ! Variables del problema
-    REAL :: xreal,breal                             ! Necesario para la función que calcula "k"
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    INTEGER :: ierr                                     ! Variable para la señal de error
+    INTEGER :: rank                                     ! ID del núcleo a usar
+    INTEGER :: nprocs                                   ! Número de procesadores a usar
+!    INTEGER, DIMENSION(MPI_STATUS_SIZE) :: status      ! Estatus de envio o recibido
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    INTEGER :: npb=1000000                              ! Cantidad de números por base
+    INTEGER :: bmin=1, bama=10, bcal                    ! Base mínima, Base máxima
+    INTEGER :: b, x, k, d, f , i                        ! Variables del problema
+    REAL :: xreal,breal                                 ! Necesario para la función que calcula "k"
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     INTEGER, DIMENSION(8) :: wvec=(/0,0,0,0,0,0,0,0/)
-    INTEGER :: a
-    CALL MPI_Init(ierr)                             ! Inicializar MPI
-    IF (ierr/=0) STOP 'MPI_Init error'              ! En caso de error se detiene el proceso
-    CALL MPI_Comm_size(MPI_COMM_WORLD,nprocs,ierr)  ! Configura el tamaño de comunicación
-    IF (ierr/=0) STOP 'MPI_Comm_size error'         ! En caso de error se detiene el proceso
-    CALL MPI_COMM_RANK(MPI_COMM_WORLD,rank,ierr)    ! Configura el ID de cada proc.
-    IF (ierr/=0) STOP 'MPI_Comm_rank error'         ! En caso de error se detiene el proceso
+    INTEGER :: suminf, sumsup                           ! Valor inferior y superior para definir el rango de bases por núcleo
+    CALL MPI_Init(ierr)                                 ! Inicializar MPI
+    IF (ierr/=0) STOP 'MPI_Init error'                  ! En caso de error se detiene el proceso
+    CALL MPI_Comm_size(MPI_COMM_WORLD,nprocs,ierr)      ! Configura el tamaño de comunicación
+    IF (ierr/=0) STOP 'MPI_Comm_size error'             ! En caso de error se detiene el proceso
+    CALL MPI_COMM_RANK(MPI_COMM_WORLD,rank,ierr)        ! Configura el ID de cada proc.
+    IF (ierr/=0) STOP 'MPI_Comm_rank error'             ! En caso de error se detiene el proceso
     bcal=bama-bmin
-    IF (bcal<=0) STOP 'Orden incorrecto de cálculo' ! Solo interesa calculo de basses ascendentes
+    IF (bcal<=0) STOP 'Orden incorrecto de cálculo'             ! Solo interesa calculo de bases ascendentes
     IF (nprocs>=9) STOP 'Más procesadores de los programados'   ! Programa para 8 o menos núcleos
     
     CALL vt(bcal,nprocs,wvec)
-    print*, wvec(1),wvec(2),wvec(3),wvec(4),wvec(5),wvec(6),wvec(7),wvec(8)
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    IF (rank==0) THEN           ! Si estamos en el núcleo 0 
-        OPEN(rank,file='Armstrong_N0.txt')           ! Abre y/o crea el archivo de texto para guardar los datos
-        IF (wvec(rank+1)==0)THEN
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    IF (rank==0) THEN                                   ! Si estamos en el núcleo 0
+        print*, wvec(1),wvec(2),wvec(3),wvec(4),wvec(5),wvec(6),wvec(7),wvec(8) ! Trabajo de cada núcleo
+        OPEN(rank,file='Armstrong_N0.txt')              ! Abre y/o crea el archivo de texto para guardar los datos
+        IF (wvec(rank+1)==0)THEN                        ! Si la cordenada 1 de wvec es cero, implica que el núcleo no trabaja
             print*, 'Núcleo ',rank,' sin uso'
-        ELSE
-                DO b=bmin, bmin+wvec(rank+1)-1          ! Desde la base mínima hasta la base máxima                  
-                    WRITE(rank,*) 'Base=',b              
-                    DO x=0, npb                         ! Se consideran solo números no negativos
-                        xreal=REAL(x)
-                        breal=REAL(b)
-                        CALL kb(xreal,breal,k)          ! Se calcula el valor de "k" y se almacena en la variable k
-                        DO i=0, k-1                     ! Se realiza una sumatoria desde i=0 hasta k-1
-                            CALL di(i,x,b,d)            ! Se calcula el factor d_i y se almacena en la variable d                  
-                            f=d**k+f                    ! Se realiza la sumatoria de d_i^k
-                        END DO
-                        IF (f==x)THEN                   ! Si f=x, este es un número de Armstrong
-                            WRITE(rank,*) f
-                        END IF
-                        f=0                             ! Se eliminan los resultados anteriores
+        ELSE                                            ! En caso contrario, el procesador trabaja
+            DO b=bmin, bmin+wvec(rank+1)-1              ! Desde la base mínima hasta la asignada
+                WRITE(rank,*) 'Base=',b              
+                DO x=1, npb                             ! Se consideran solo números positivos
+                    xreal=REAL(x)
+                    breal=REAL(b)
+                    CALL kb(xreal,breal,k)              ! Se calcula el valor de "k" y se almacena en la variable k
+                    DO i=0, k-1                         ! Se realiza una sumatoria desde i=0 hasta k-1
+                        CALL di(i,x,b,d)                ! Se calcula el factor d_i y se almacena en la variable d                  
+                        f=d**k+f                        ! Se realiza la sumatoria de d_i^k
                     END DO
+                    IF (f==x)THEN                       ! Si f=x, este es un número de Armstrong
+                        WRITE(rank,*) f                 ! y se almacena en un archivo .txt
+                    END IF
+                    f=0                                 ! Se eliminan los resultados anteriores
                 END DO
-        END IF
-    ELSE IF (rank==1) THEN      ! Si estamos en el núcleo 1 
-        IF (wvec(rank+1)==0)THEN
-            print*, 'Núcleo ',rank,' sin uso'
-        ELSE
-            DO a=1, wvec(rank+1)
-                print*, a
             END DO
         END IF
-    ELSE IF (rank==2) THEN      ! Si estamos en el núcleo 2
+    ELSE IF (rank==1) THEN                              ! Si estamos en el núcleo 1 
+        OPEN(rank,file='Armstrong_N1.txt')              
         IF (wvec(rank+1)==0)THEN
             print*, 'Núcleo ',rank,' sin uso'
         ELSE
-            DO a=1, wvec(rank+1)
-                
+            suminf=bmin+wvec(rank)
+            sumsup=bmin+wvec(rank)+wvec(rank+1)-1
+            DO b=suminf,sumsup                          ! Rango de las bases asignadas al núcleo
+                WRITE(rank,*) 'Base=',b
+                DO x=1, npb
+                    xreal=REAL(x)
+                    breal=REAL(b)
+                    CALL kb(xreal,breal,k)
+                    DO i=0, k-1
+                        CALL di(i,x,b,d)
+                        f=d**k+f
+                    END DO
+                    IF (f==x)THEN
+                        WRITE(rank,*) f
+                    END IF
+                    f=0
+                END DO
             END DO
-        END IF    
-    ELSE IF (rank==3) THEN      ! Si estamos en el núcleo 3   
+        END IF
+    ELSE IF (rank==2) THEN                              ! Si estamos en el núcleo 2
+        OPEN(rank,file='Armstrong_N2.txt')
         IF (wvec(rank+1)==0)THEN
             print*, 'Núcleo ',rank,' sin uso'
         ELSE
-            DO a=1, wvec(rank+1)
-                
+            suminf=bmin+wvec(rank)+wvec(rank-1)
+            sumsup=bmin+wvec(rank-1)+wvec(rank)+wvec(rank+1)-1
+            DO b=suminf,sumsup                          ! Rango de las bases asignadas
+                WRITE(rank,*) 'Base=',b              
+                DO x=1, npb
+                    xreal=REAL(x)
+                    breal=REAL(b)
+                    CALL kb(xreal,breal,k)
+                    DO i=0, k-1
+                        CALL di(i,x,b,d)
+                        f=d**k+f
+                    END DO
+                    IF (f==x)THEN
+                        WRITE(rank,*) f
+                    END IF
+                    f=0
+                END DO
             END DO
-        END IF    
-    ELSE IF (rank==4) THEN      ! Si estamos en el núcleo 4   
+        END IF
+    ELSE IF (rank==3) THEN                              ! Si estamos en el núcleo 3
+        OPEN(rank,file='Armstrong_N3.txt')
         IF (wvec(rank+1)==0)THEN
             print*, 'Núcleo ',rank,' sin uso'
         ELSE
-            DO a=1, wvec(rank+1)
-                
+            suminf=bmin+wvec(rank)+wvec(rank-1)+wvec(rank-2)
+            sumsup=bmin+wvec(rank-2)+wvec(rank-1)+wvec(rank)+wvec(rank+1)-1
+            DO b=suminf,sumsup
+                WRITE(rank,*) 'Base=',b
+                DO x=1, npb
+                    xreal=REAL(x)
+                    breal=REAL(b)
+                    CALL kb(xreal,breal,k)
+                    DO i=0, k-1
+                        CALL di(i,x,b,d)
+                        f=d**k+f
+                    END DO
+                    IF (f==x)THEN
+                        WRITE(rank,*) f
+                    END IF
+                    f=0
+                END DO
             END DO
-        END IF    
-    ELSE IF (rank==5) THEN      ! Si estamos en el núcleo 5   
+        END IF 
+    ELSE IF (rank==4) THEN                            ! Si estamos en el núcleo 4
+        OPEN(rank,file='Armstrong_N4.txt')
         IF (wvec(rank+1)==0)THEN
             print*, 'Núcleo ',rank,' sin uso'
         ELSE
-            DO a=1, wvec(rank+1)
-                
+            suminf=bmin+wvec(rank)+wvec(rank-1)+wvec(rank-2)+wvec(rank-3)
+            sumsup=bmin+wvec(rank-3)+wvec(rank-2)+wvec(rank-1)+wvec(rank)+wvec(rank+1)-1
+            DO b=suminf,sumsup
+                WRITE(rank,*) 'Base=',b
+                DO x=1, npb
+                    xreal=REAL(x)
+                    breal=REAL(b)
+                    CALL kb(xreal,breal,k)
+                    DO i=0, k-1
+                        CALL di(i,x,b,d)
+                        f=d**k+f
+                    END DO
+                    IF (f==x)THEN
+                        WRITE(rank,*) f
+                    END IF
+                    f=0
+                END DO
             END DO
-        END IF    
-    ELSE IF (rank==6) THEN      ! Si estamos en el núcleo 6   
+        END IF
+    ELSE IF (rank==5) THEN                          ! Si estamos en el núcleo 5
+        OPEN(rank,file='Armstrong_N5.txt')
         IF (wvec(rank+1)==0)THEN
             print*, 'Núcleo ',rank,' sin uso'
         ELSE
-            DO a=1, wvec(rank+1)
-                
+            suminf=bmin+wvec(rank)+wvec(rank-1)+wvec(rank-2)+wvec(rank-3)+wvec(rank-4)
+            sumsup=bmin+wvec(rank-4)+wvec(rank-3)+wvec(rank-2)+wvec(rank-1)+wvec(rank)+wvec(rank+1)-1
+            DO b=suminf,sumsup
+                WRITE(rank,*) 'Base=',b
+                DO x=1, npb
+                    xreal=REAL(x)
+                    breal=REAL(b)
+                    CALL kb(xreal,breal,k)
+                    DO i=0, k-1
+                        CALL di(i,x,b,d)
+                        f=d**k+f
+                    END DO
+                    IF (f==x)THEN
+                        WRITE(rank,*) f
+                    END IF
+                    f=0
+                END DO
             END DO
-        END IF    
-    ELSE IF (rank==7) THEN      ! Si estamos en el núcleo 7   
+        END IF
+    ELSE IF (rank==6) THEN                          ! Si estamos en el núcleo 6
+        OPEN(rank,file='Armstrong_N6.txt')
         IF (wvec(rank+1)==0)THEN
             print*, 'Núcleo ',rank,' sin uso'
         ELSE
-            DO a=1, wvec(rank+1)
-                
+            suminf=bmin+wvec(rank)+wvec(rank-1)+wvec(rank-2)+wvec(rank-3)+wvec(rank-4)+wvec(rank-5)
+            sumsup=bmin+wvec(rank-5)+wvec(rank-4)+wvec(rank-3)+wvec(rank-2)+wvec(rank-1)+wvec(rank)+wvec(rank+1)-1
+            DO b=suminf,sumsup
+                WRITE(rank,*) 'Base=',b
+                DO x=1, npb
+                    xreal=REAL(x)
+                    breal=REAL(b)
+                    CALL kb(xreal,breal,k)
+                    DO i=0, k-1
+                        CALL di(i,x,b,d)
+                        f=d**k+f
+                    END DO
+                    IF (f==x)THEN
+                        WRITE(rank,*) f
+                    END IF
+                    f=0
+                END DO
             END DO
-        END IF    
+        END IF
+    ELSE IF (rank==7) THEN                          ! Si estamos en el núcleo 7
+        OPEN(rank,file='Armstrong_N7.txt')
+        IF (wvec(rank+1)==0)THEN
+            print*, 'Núcleo ',rank,' sin uso'
+        ELSE
+            suminf=bmin+wvec(rank)+wvec(rank-1)+wvec(rank-2)+wvec(rank-3)+wvec(rank-4)+wvec(rank-5)+wvec(rank-6)
+            sumsup=bmin+wvec(rank-6)+wvec(rank-5)+wvec(rank-4)+wvec(rank-3)+wvec(rank-2)+wvec(rank-1)+wvec(rank)+wvec(rank+1)-1
+            DO b=suminf,sumsup
+                WRITE(rank,*) 'Base=',b
+                DO x=1, npb
+                    xreal=REAL(x)
+                    breal=REAL(b)
+                    CALL kb(xreal,breal,k)
+                    DO i=0, k-1
+                        CALL di(i,x,b,d)
+                        f=d**k+f
+                    END DO
+                    IF (f==x)THEN
+                        WRITE(rank,*) f
+                    END IF
+                    f=0
+                END DO
+            END DO
+        END IF
     END IF
-
-    Call MPI_FINALIZE(ierr)         ! Finaliza MPI    
+    Call MPI_FINALIZE(ierr)                         ! Finaliza MPI    
 END PROGRAM ArmstrongParalelo
 
 
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-SUBROUTINE di(i,n,b,y)                      ! Subrutina para encontrar el coeficiente d_i
+SUBROUTINE di(i,n,b,y)                              ! Subrutina para encontrar el coeficiente d_i
     INTEGER, INTENT(IN) :: i, n, b
     INTEGER, INTENT(OUT) :: y
-    y=(MOD(n,b**(i+1))-MOD(n,b**i))/(b**i)  ! Fórmula dada para calcular
+    y=(MOD(n,b**(i+1))-MOD(n,b**i))/(b**i)          ! Fórmula dada para calcular
 END SUBROUTINE di
 
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-SUBROUTINE kb(x,b,y)                        ! Subrutina para encontrar el coeficiente k
+SUBROUTINE kb(x,b,y)                                ! Subrutina para encontrar el coeficiente k
     REAL, INTENT(IN) :: b,x
     INTEGER, INTENT(OUT) :: y
     REAL :: z
-    z=LOG(x)/LOG(b)                         ! Formula para el cambio de logaritmo a base b
-    y=INT(z)+1                              ! El resultado es un entero y se devuelve como tal
+    z=LOG(x)/LOG(b)                                 ! Formula para el cambio de logaritmo a base b
+    y=INT(z)+1                                      ! El resultado es un entero y se devuelve como tal
 END SUBROUTINE kb
 
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-SUBROUTINE vt(b,c,y)
+SUBROUTINE vt(b,c,y)                                ! Subrutina para crear el vector que asigna trabajo a cada núcleo
     INTEGER, INTENT(IN) ::  b, c
     INTEGER, DIMENSION(8), INTENT(OUT) :: y
     INTEGER :: a, rond, paso
-    IF (c==(b-1)) THEN
-        DO a=1, c
+    IF (c==b) THEN                                  ! Si el número de núcleos es igual al número de bases, todos hacen 
+        DO a=1, c                                   ! una operación, se asigna el valor 1 a todas las coordenadas
             y(a)=1
         END DO
-    ELSE IF (c>b) THEN
-        DO a=1, b+1
+    ELSE IF (c>b) THEN                              ! Si el número de núcleos es mayor al número de bases b, solo hacen 
+        DO a=1, b+1                                 ! una operación los primeros b núcleos, se asigna 1 a estas coordenadas
             y(a)=1
         END DO
-    ELSE IF(c<=b)THEN
-        rond=FLOOR(REAL(b)/REAL(c))
-        DO a=1, c
+    ELSE IF(c<=b)THEN                               ! Si el número de núcleos es menor al número de bases b, entonces
+        rond=FLOOR(REAL(b)/REAL(c))                 ! El número aproximado de la división entre el número de procesadores/b es 
+        DO a=1, c                                   ! la cantidad de procesos que todos los núcleos van a realizar
             y(a)=1*rond
-        END DO
-        paso=MOD(b,c)+1
+        END DO                                      ! El resto dado por MOD(), se distribuye entre los núcleos necesarios 
+        paso=MOD(b,c)+1                             ! uno a la vez
         DO a=1, paso
             y(a)=y(a)+1
         END DO
     END IF
 END SUBROUTINE vt
 
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
